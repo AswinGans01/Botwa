73900d8b7662f7ba7df5d5bdc4e33e6ccfeceb95
AswinGans01 <76236532+AswinGans01@users.noreply.github.com>
2020-12-19 13:13:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'Dark Sky BOT☄'; 
+const instagram = 'Aswin_id'; 
+const telegram = 'Aswin Danendra'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
 //const grupch1 = 'belum ada grup'; 
